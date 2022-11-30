d7f37861ba470d9864be5dbd394caf0000e9bb2d
rafael.pierre@databricks.com <rafael.pierre@databricks.com>
2022-11-30 14:27:50 +0000

    final

--- notebooks/03_train.py
+++ notebooks/03_train.py
@@ -2,6 +2,11 @@
 # MAGIC %md
 # MAGIC 
 # MAGIC ## Distilbert Example
+# MAGIC 
+# MAGIC <hr />
+# MAGIC 
+# MAGIC * In the cell below, we simply download a pre-trained, distilled bersion of BERT (`distilbert-base-uncased`) to see how it works.
+# MAGIC * We go on and run a sample prediction for a piece of text
 
 # COMMAND ----------
 
@@ -13,10 +18,18 @@ model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-unc
 
 inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
 with torch.no_grad():
-    logits = model(**inputs).logits
+  outputs = model(**inputs, labels = torch.tensor(0))
+  logits = outputs.logits
+  loss = outputs.loss
 
 predicted_class_id = logits.argmax().item()
 model.config.id2label[predicted_class_id]
+print(loss)
+
+# COMMAND ----------
+
+# DBTITLE 1,Model Architecture
+model
 
 # COMMAND ----------
 
@@ -25,6 +38,7 @@ model.config.id2label[predicted_class_id]
 # MAGIC 
 # MAGIC * Our model must have 12 different labels; we need to customize `DistilBERT` to support that
 # MAGIC * But first, we need to have a Torch `Dataset` and a `DataLoader`. Let's code that up
+# MAGIC * We will do this in a way that we directly query data that is stored in a Delta table
 
 # COMMAND ----------
 
@@ -43,121 +57,278 @@ class InsuranceDataset(Dataset):
       label_col = "topic_en",
       storage_level = StorageLevel.MEMORY_ONLY,
       tokenizer = "distilbert-base-uncased",
-      max_length = 50
+      max_length = 512
     ):
+      super().__init__()
       self.input_col = input_col
       self.label_col = label_col
       self.df = spark.sql(
         f"select * from {database_name}.{split}"
-      ).persist(storage_level)
-      self.length = self.df.count()
+      ).toPandas()
+      self.length = len(self.df)
       self.class_mappings = self._get_class_mappings()
       self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
       self.max_length = max_length
 
     def _get_class_mappings(self):
       labels = self.df \
-        .select(f"topic_en") \
-        .distinct() \
-        .toPandas() \
-        .iloc[:, 0]
+        .topic_en \
+        .unique()
 
       indexes = LabelEncoder().fit_transform(labels)
       class_mappings = dict(zip(labels, indexes))
       return class_mappings
 
+    def _encode_label(self, label):
+      self._get_class_mappings()
+      label_class = self.class_mappings[label]
+      encoded_label = torch.nn.functional.one_hot(
+        torch.tensor([label_class]),
+        num_classes = len(self.class_mappings)
+      )
+      return encoded_label.type(torch.float)
+
     def __len__(self):
-      return self.length
+      return len(self.df)
 
     def __getitem__(self, idx):
-      query = f"id = {idx}"
-      df = self.df.filter(query).toPandas()
-      question = df.loc[0, self.input_col]
+      question = self.df.loc[idx, self.input_col]
       inputs = self.tokenizer(
         question,
+        None,
         add_special_tokens=True,
-        max_length=self.max_length,
-        padding='max_length',
         return_token_type_ids=True,
-        truncation=True
+        truncation=True,
+        max_length=self.max_length,
+        padding="max_length"
       )
 
       ids = inputs['input_ids']
       mask = inputs['attention_mask']
-      label = self.class_mappings[df.loc[0, self.label_col]]
-
+      labels = self.df.loc[idx, self.label_col]
+      labels = self._encode_label(labels)[0]
       return {
-        'ids': torch.tensor(ids, dtype=torch.long),
-        'mask': torch.tensor(mask, dtype=torch.long),
-        'targets': torch.tensor(label, dtype=torch.long)
+        "input_ids": torch.tensor(ids, dtype = torch.long),
+        "attention_mask": torch.tensor(mask, dtype = torch.long),
+        "labels": labels
       }
 
 # COMMAND ----------
 
-from torch.utils.data import DataLoader
+# MAGIC %md
+# MAGIC 
+# MAGIC Let's see how our dataset samples look like:
+
+# COMMAND ----------
 
 training_data = InsuranceDataset(split = "train")
-test_data = InsuranceDataset(split = "test")
+training_data[100]
 
-training_data[0]
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Data Loaders
+# MAGIC 
+# MAGIC <hr />
+# MAGIC 
+# MAGIC * PyTorch provides DataLoaders, which are a simple way to provide a batching interface on top of your datasets.
+# MAGIC * They can be configured to work on a distributed fashion, by setting the `num_workers` parameter
 
 # COMMAND ----------
 
 # DBTITLE 1,Instantiating Data Loaders
 from torch.utils.data import DataLoader
 
-train_dataloader = DataLoader(training_data, batch_size=4, shuffle=True)
-test_dataloader = DataLoader(test_data, batch_size=4, shuffle=True)
+test_data = InsuranceDataset(split = "test")
+train_dataloader = DataLoader(training_data, batch_size=32, shuffle=True, num_workers=20)
+test_dataloader = DataLoader(test_data, batch_size=16, shuffle=False, num_workers=20)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Lightning Modules
+# MAGIC 
+# MAGIC <hr />
+# MAGIC 
+# MAGIC * Time to declare our Lightning Module!
+# MAGIC * Lightning Module is quite a useful class provided by PyTorch Lightning. It helps us avoid a lot of boiler plate code - and also avoid forgetting about `torch.no_grad()` when running forward pass 😃
+# MAGIC * We can customize how our model is going to be loaded, which layers in the model we are going to train or not, how metrics will be logged, etc.
 
 # COMMAND ----------
 
 import pytorch_lightning as pl
 import torch.nn as nn
 import torch.nn.functional as F
+from transformers import AdamW, AutoConfig, AutoModelForSequenceClassification
+from typing import Optional
+import numpy as np
+from pytorch_lightning.utilities import rank_zero_only
 
 class LitModel(pl.LightningModule):
-    def __init__(self, model = "distilbert-base-uncased"):
+    def __init__(
+      self,
+        model_name_or_path: str = "distilbert-base-uncased",
+        num_labels: int = 12,
+        learning_rate: float = 1e-5,
+        adam_epsilon: float = 1e-8,
+        warmup_steps: int = 0,
+        weight_decay: float = 1e-10,
+        eval_splits: Optional[list] = None,
+        freeze_layers = True,
+        **kwargs,
+    ):
       super().__init__()
-      self.l1 = DistilBertForSequenceClassification.from_pretrained(model)
-      self.pre_classifier = torch.nn.Linear(768, 768)
-      self.dropout = torch.nn.Dropout(0.3)
-      self.classifier = torch.nn.Linear(768, 12)
-
-    def forward(self, sample):
-      print(sample)
-      output_1 = self.l1(
-        input_ids = sample["ids"],
-        attention_mask = sample["mask"]
+      self.save_hyperparameters()
+      self.config = AutoConfig.from_pretrained(
+        model_name_or_path,
+        num_labels = num_labels,
+        problem_type = "multi_label_classification"
       )
-      hidden_state = output_1[0]
-      pooler = hidden_state[:, 0]
-      pooler = self.pre_classifier(pooler)
-      pooler = torch.nn.ReLU()(pooler)
-      pooler = self.dropout(pooler)
-      output = self.classifier(pooler)
-      return output
+      self.l1 = AutoModelForSequenceClassification.from_pretrained(
+        model_name_or_path,
+        config = self.config
+      )
+
+      if freeze_layers:
+        for name, param in self.l1.named_parameters():
+          if "distilbert" in name:
+              param.requires_grad = False
+
+    def forward(self, **inputs):
+      output_l1 = self.l1(**inputs)
+      return output_l1
+
+    @rank_zero_only
+    def _log_metrics(self, key, values):
+      value = np.mean(values)
+      self.logger.experiment.log_metric(
+        key = key,
+        value = value,
+        run_id = self.logger.run_id
+      )
 
     def training_step(self, batch, batch_idx):
-      x, y = batch
-      y_hat = self(x)
-      loss = F.cross_entropy(y_hat, y)
-      return loss
+      outputs = self(**batch)
+      self.log("loss", outputs.loss)
+      return {"loss": outputs.loss}
+
+    def training_epoch_end(self, outputs):
+      all_preds = [output["loss"].cpu().numpy() for output in outputs]
+      self._log_metrics("loss", all_preds)
+
+    def validation_step(self, batch, batch_idx, dataloader_idx = 0):
+      outputs = self(**batch)
+      metric = {"val_loss": outputs.loss}
+      self.log("val_loss", outputs.loss)
+      return metric
+
+    def validation_epoch_end(self, outputs):
+      all_preds = [output["val_loss"].cpu().numpy() for output in outputs]
+      self.log("val_loss", np.mean(all_preds))
+      self._log_metrics("val_loss", all_preds)
+
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):
+      y_hat = self.model(**batch)
+      return y_hat
 
     def configure_optimizers(self):
-      return torch.optim.Adam(self.parameters(), lr=0.02)
+      return AdamW(self.parameters(), lr=1e-5)
 
 # COMMAND ----------
 
-lit_model = LitModel()
-trainer = pl.Trainer(accelerator = "gpu")
+# MAGIC %md
+# MAGIC 
+# MAGIC ## Training our Model
+# MAGIC 
+# MAGIC Here we will:
+# MAGIC 
+# MAGIC * Declare an `EarlyStoppingCallback` - this way we can avoid overfitting and don't need to think about number of training epochs
+# MAGIC * Declare an `MLFlowLogger` - since MLFlow `autologging` doesn't work for PyTorch Lightning > 1.5
+# MAGIC * Create a `Trainer` to train our model
+# MAGIC * Train our model!
 
 # COMMAND ----------
 
-lit_model(next(iter(training_data)))
+import mlflow
+from pytorch_lightning.loggers import MLFlowLogger
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
+early_stop_callback = EarlyStopping(
+  monitor="val_loss",
+  min_delta=0.001,
+  patience=3,
+  verbose=True,
+  mode="min"
+)
+
+with mlflow.start_run(run_name = "torch") as run:
+  lit_model = LitModel()
+  run_id = run.info.run_id
+  experiment_name = mlflow.get_experiment(run.info.experiment_id)
+
+  mlf_logger = MLFlowLogger(
+      experiment_name = experiment_name,
+      run_id=run_id,
+      tracking_uri="databricks"
+  )
+
+  trainer = pl.Trainer(
+    max_epochs = 10000,
+    logger = mlf_logger,
+    accelerator="gpu",
+    devices = 4,
+    callbacks = [early_stop_callback]
+  )
+
+  trainer.fit(
+    lit_model,
+    train_dataloaders = train_dataloader,
+    val_dataloaders = test_dataloader
+  )
 
 # COMMAND ----------
 
-trainer.fit(lit_model, train_dataloader, test_dataloader)
+def predict(question = "how do i get car insurance?", ground_truth = "auto-insurance"):
+
+  encodings = tokenizer(
+    question,
+    None,
+    add_special_tokens=True,
+    return_token_type_ids=True,
+    truncation=True,
+    max_length=512,
+    padding="max_length"
+  )
+
+  inputs = {
+    "input_ids": torch.tensor([encodings["input_ids"]], dtype = torch.long),
+    "attention_mask": torch.tensor([encodings["attention_mask"]], dtype = torch.long)
+  }
+
+  with torch.no_grad():
+    y_hat = lit_model(**inputs)
+
+  label = y_hat.logits.argmax().item()
+  return label
+
+# COMMAND ----------
+
+sample["question_en"].values
+
+# COMMAND ----------
+
+# DBTITLE 1,Testing our model
+for i in range(0, 10):
+  sample = test_data.df.sample(1)
+  question = sample["question_en"].values[0]
+  topic = sample["topic_en"].values[0]
+
+  pred = predict(question, topic)
+  print(f"Question: {question}")
+  print(f"Predicted '{topic}'? {pred == training_data.class_mappings[topic]}")
+  print(f"------------------------------------------------------------------")
 
 # COMMAND ----------
 
